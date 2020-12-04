3be7660d6d8fcc86272d4d088b23781e509869ce
Hashim <hashimc@23andme.com>
2020-12-04 00:58:39 -0800

    Day  3, part B completed

--- 2020/day3.py
+++ 2020/day3.py
@@ -1,25 +1,31 @@
 import sys
 
 
-def partB(inputs: list) -> int:
-    return 0
-
-
 if __name__ == "__main__":
-    index = 3
-    trees = 0
-    add = False
+    slopes = [(1, 2), (7, 1), (1, 1), (5, 1), (3, 1)]
+    index_tracker = [1, 7, 1, 5, 3]
+    skip_tracker = [2, 1, 1, 1, 1]
+    trees = [0] * 5
     with open('inputs/day3.txt') as f:
+        lines = 0
         for line in f:
-            if not add:
-                add = not add
-                continue
-            row = list(line.strip())
-            index = index % len(row)
-            if row[index] == '#':
-                trees += 1
-            index += 3
+            for i in range(len(slopes)):
+                base_index = slopes[i][0]
+                base_skip = slopes[i][1]
+                if skip_tracker[i]:
+                    skip_tracker[i] = skip_tracker[i] - 1
+                    continue
+                row = list(line.strip())
+                if row[index_tracker[i] % len(row)] == '#':
+                    trees[i] += 1
+                index_tracker[i] += base_index
+                if base_skip == 2:
+                    skip_tracker[i] += 1
+            lines += 1
     if sys.argv[1] == 'A':
-        print(trees)
+        print(trees[-1])
     else:
-        pass
+        result = 1
+        for tree in trees:
+            result = result * tree
+        print(result)
