38ad1201db9f9f7ad4107ca3bd856954c974e041
Hashim <hashimc@23andme.com>
2020-12-08 01:35:51 -0800

    comments added

--- 2020/day6.py
+++ 2020/day6.py
@@ -1,22 +1,22 @@
 from functools import reduce
 
 if __name__ == "__main__":
-    next = False
-    result = 0
+    result_a = 0
     result_b = 0
     with open('inputs/day6.txt') as f:
-        ans = set()
+        ans_a = set()
         ans_b = []
         for line in f:
             if line == "\n":
-                result += len(ans)
+                result_a += len(ans_a)
+                # bitwise and operation on set gives us interesctions
                 common = reduce(lambda x, y: x & y, ans_b)
                 result_b += len(common)
-                ans = set()
+                ans_a = set()
                 ans_b = []
                 continue
             s = line.strip()
-            ans.update(list(s))
+            ans_a.update(list(s))
             ans_b.append(set(list(s)))
-    print(result)
+    print(result_a)
     print(result_b)
