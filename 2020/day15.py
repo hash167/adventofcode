330cd1f7786bde7f76129807be0eec8069205d43
Hashim <hashimc@23andme.com>
2020-12-17 20:38:41 -0800

    Day 16 completed

--- 2020/day15.py
+++ 2020/day15.py
@@ -1,27 +1,36 @@
+from collections import defaultdict
 def partB(sn, max_turn):
     turn = 0
     prev = 0
     mem = {}
     for b in sn:
-        mem[b] = [turn, turn]
-        turn += 1
+        mem[b] = turn
         prev = b
+        turn += 1
     while turn < max_turn:
-        if mem[prev][0] == mem[prev][1]:
-            prev = 0
-        else:
-            prev = mem[prev][1] - mem[prev][0]
+        val = None
         if prev in mem:
-            mem[prev][0] = mem[prev][1]
-            mem[prev][1] = turn
+            val = (turn - 1) - mem[prev]
         else:
-            mem[prev] = [turn, turn]
+            val = 0
+        if prev >= 0:
+            mem[prev] = turn - 1 
+        prev = val
         turn += 1
     return prev
+
+
+def partB_concise(sn, max_turn):
+    last_seen = defaultdict(lambda: i, {c: i for i, c in enumerate(sn[:-1])})
+    last = sn[-1]
+    turn = len(sn) - 1
+    for i in range(turn, max_turn - 1):
+        last_seen[last], last = i, i - last_seen[last]
+    return last
 
 
 if __name__ == "__main__":
     inputs = [line.strip().split(",") for line in open('inputs/day15.txt').readlines()][0]
     starting_numbers = [int(i) for i in inputs]
-    print(partB(starting_numbers, 2020))
-    print(partB(starting_numbers, 30000000))
+    print(partB_concise(starting_numbers, 2020))
+    print(partB_concise(starting_numbers, 30000000))
---
+++ 2020/day16.py
@@ -0,0 +1,110 @@
+import itertools
+from collections import defaultdict
+
+# Not needed but added it, just in case it was
+def combine_ranges(sorted_ranges):
+    res = [sorted_ranges[0]]
+    for i in range(1, len(sorted_ranges)):
+        prev = res.pop()
+        if prev[1] >= sorted_ranges[i][0]:
+            if prev[1] >= sorted_ranges[i][1]:
+                res.append(prev)
+            else:
+                res.append([prev[0], sorted_ranges[i][1]])
+        else:
+            res.append(prev)
+            res.append(sorted_ranges[i])
+    return res
+
+
+def in_range(item: int, el_list: list) -> bool:
+    inrange = False
+    for el in el_list:
+        # print(el)
+        if el[0] <= item <= el[1]:
+            inrange = True
+    return inrange
+
+
+def partA(rules, my, nearby):
+    ranges = list(itertools.chain.from_iterable(rules.values()))
+    ranges = sorted(ranges, key=lambda x: x[0])
+    ranges = combine_ranges(ranges)
+    invalid = []
+    valid = []
+    for n in nearby:
+        valid_ticket = True
+        for i in n:
+            if i in invalid or not in_range(i, ranges):
+                invalid.append(i)
+                valid_ticket = False
+                break
+        if valid_ticket:
+            valid.append(n)
+    print(invalid)
+    return invalid, valid
+
+
+def partB(rules, my, valid):
+    values_at_pos = defaultdict(list)
+    for ticket in valid:
+        for i, num in enumerate(ticket):
+            values_at_pos[i].append(num)
+    rules_mapping = defaultdict(dict)
+    for pos, values in values_at_pos.items():
+        for name, ranges in rules.items():
+            cont_outer = False
+            for item in values:
+                if not in_range(item, ranges):
+                    cont_outer = True
+                    break
+            if cont_outer:
+                continue
+            rules_mapping[name][pos] = True
+    result = {}
+    while rules_mapping:
+        for name, pos in rules_mapping.copy().items():
+            if len(pos) != 1:
+                continue
+            p = list(pos.keys())[0]
+            result[name] = p
+            del rules_mapping[name]
+            for key, val in rules_mapping.items():
+                if p in val:
+                    del val[p]
+
+    prod = 1
+    for name, index in result.items():
+        if 'departure' in name:
+            prod *= my[index]
+    return prod
+
+
+
+if __name__ == "__main__":
+    section = 0
+    my_ticket = []
+    nearby_tickets = []
+    rules = {}
+    with open('inputs/day16.txt') as f:
+        for line in f:
+            if not line.strip():
+                continue
+            if line.strip() == "your ticket:":
+                section += 1
+                continue
+            elif line.strip() == "nearby tickets:":
+                section += 1
+                continue
+            if section == 0:
+                rule_id, values = line.strip().split(": ")
+                values = values.split(' or ')
+                values = [[int(i) for i in v.split('-')] for v in values]
+                rules[rule_id] = values
+            if section == 1:
+                my_ticket = [int(v) for v in line.strip().split(',')]
+            if section == 2:
+                nearby_tickets.append([int(v) for v in line.strip().split(',')])
+    invalid_set, valid = partA(rules, my_ticket, nearby_tickets)
+    print(sum(invalid_set))
+    print(partB(rules, my_ticket, valid))
